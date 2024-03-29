ed3aabb78e0ea9024f9a7d906cd5539a8221fef3
RJones0421 <orangestreak0421@gmail.com>
2024-03-29 15:36:53 -0400

    Display Win32 in WPF

---
+++ Win32WPF/WindowAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win32WPF
+{
+  public class WindowAPI
+  {
+    private const string windowDLL = @"D:\WPFTest\Win32WPF\x64\Debug\CppDll.dll";
+
+    [DllImport(windowDLL)]
+    public static extern IntPtr InitializeWin32(IntPtr host, int width, int height);
+  }
+}
